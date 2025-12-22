cbc6a0e604783aeb04edb8bcc6a9d66a729c8292
Maheshh-S <mahesh06aa@gmail.com>
2025-12-22 22:23:16 +0530

    feat: add base app shell and global styles

---
+++ app/layout.tsx
@@ -0,0 +1,55 @@
+import { type Metadata } from "next";
+import { Poppins } from "next/font/google";
+import clsx from "clsx";
+import { Toaster } from "react-hot-toast";
+
+import "@/styles/tailwind.css";
+import { Providers } from "@/components/providers/theme-providers";
+import { QueryClientProvider } from "@/components/providers/query-client-provider";
+import CartProvider from "@/providers/cart-provider";
+import NetworkIndicator from "@/components/global/network-indicator";
+import { Suspense } from "react";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["200", "400"],
+});
+
+export const metadata: Metadata = {
+  title: "Vendary - A shopping experience like no other",
+  description:
+    "Vendary is a shopping platform that offers a unique and personalized shopping experience, combining the best of online and offline shopping.",
+  alternates: {
+    types: {
+      "application/rss+xml": `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
+    },
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html
+      lang="en"
+      className={clsx("h-full antialiased", font.className)}
+      suppressHydrationWarning
+    >
+      <QueryClientProvider>
+        <body className="flex min-h-full flex-col dark:bg-gray-950">
+          <Providers>
+            <CartProvider>
+              <Suspense fallback={null}>
+                <NetworkIndicator />
+              </Suspense>
+              {children}
+            </CartProvider>
+          </Providers>
+          <Toaster position="top-center" reverseOrder={false} />
+        </body>
+      </QueryClientProvider>
+    </html>
+  );
+}
