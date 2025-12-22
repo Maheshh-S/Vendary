ad56bd91c981fdb468b389d379d3d885cc13fa1d
Maheshh-S <mahesh06aa@gmail.com>
2025-12-22 21:56:00 +0530

    chore: initialize project configuration

---
+++ next.config.ts
@@ -0,0 +1,40 @@
+import nextMDX from '@next/mdx'
+
+import { recmaPlugins } from './mdx/recma.mjs'
+import { rehypePlugins } from './mdx/rehype.mjs'
+import { remarkPlugins } from './mdx/remark.mjs'
+
+const withMDX = nextMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins,
+    rehypePlugins,
+    recmaPlugins,
+  },
+})
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https" as const,
+        hostname: "tailwindcss.com",
+      },
+      {
+        protocol: "https" as const,
+        hostname: "m.media-amazon.com"
+      },
+      {
+        protocol: "https" as const,
+        hostname: "lh3.googleusercontent.com"
+      },
+      { 
+        protocol: "https" as const, 
+        hostname: "images.pexels.com" }
+    ]
+  }
+}
+
+export default withMDX(nextConfig)
